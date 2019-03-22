321136e88cfb1afb10dd031c513eeed4e8dff686
Wandson <wandson@live.com>
2019-03-21 21:50:35 -0300

    addtelas21/03

--- src/telas/principal.java
+++ src/telas/principal.java
@@ -1,7 +1,7 @@
 package telas;
 public class principal {
     public static void main(String[] args) {
-        
+        telalogin login = new telalogin();
+        login.setVisible(true);       
     }
-    
 }
